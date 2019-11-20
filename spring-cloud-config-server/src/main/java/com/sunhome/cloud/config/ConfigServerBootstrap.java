609ef63b806d8de863182fe57fbe0295b793d3bc
qijia.wang@renren-inc.com <qijia.wang@renren-inc.com>
2019-11-20 14:02:19 +0800

    change project package

--- spring-cloud-config-server/src/main/java/com/sunhome/cloud/config/ConfigServerBootstrap.java
+++ spring-cloud-config-server/src/main/java/com/sunhome/cloud/config/ConfigServerBootstrap.java
@@ -2,12 +2,12 @@ package com.sunhome.cloud.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @EnableConfigServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigServerBootstrap {
 
